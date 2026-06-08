4c8ce093caea537b32774c540cddf487bbf79464
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-08 12:19:30 -0300

    Initial commit

---
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ return <div>
+  <h1>Meu Projeto React</h1>
+  <button>Get advice</button>
+  </div>
+  
+}
+
+export default App
