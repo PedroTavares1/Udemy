548118746b64c2ba2de5b80a7821437da491d50e
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-08 13:57:20 -0300

    2. Building Our First React App!
    2. Building Our First React App!

--- The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
@@ -2,10 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- return <div>
-  <h1>Meu Projeto React</h1>
-  <button>Get advice</button>
-  </div>
+ 
+  const [advice, setAdvice] = useState('') // Estado para armazenar o conselho recebido da API
+  const [id, setId] = useState(0) // Estado para armazenar o ID do conselho recebido da API
+
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json()
+    console.log(data.slip.advice)
+    setId(data.slip.id)
+    setAdvice(data.slip.advice)
+  }
+  
+ return (
+    <div>
+      <h1>Meu Projeto React</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <h2>{id} - {advice}</h2>
+    </div>
+  )
   
 }
 
