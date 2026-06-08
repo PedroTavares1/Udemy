7998ce7dbae673b0185ea345ec49bce0e04d44d3
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-08 15:33:32 -0300

    2. Building Our First React App!
    Parte 2, finalizando tarefa.

--- The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
  
   const [advice, setAdvice] = useState('') // Estado para armazenar o conselho recebido da API
   const [id, setId] = useState(0) // Estado para armazenar o ID do conselho recebido da API
+  const [count, setCount] = useState(0) // Estado para contar quantos conselhos foram lidos
+
 
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice')
@@ -12,16 +14,32 @@ function App() {
     console.log(data.slip.advice)
     setId(data.slip.id)
     setAdvice(data.slip.advice)
+    setCount((c) => c + 1)
   }
   
+  useEffect(function() {
+    getAdvice()
+  }, []) // O array vazio [] garante que a função seja executada apenas uma vez, quando o componente for montado.
+
  return (
     <div>
       <h1>Meu Projeto React</h1>
       <button onClick={getAdvice}>Get advice</button>
       <h2>{id} - {advice}</h2>
+      <Message count={count} />
     </div>
   )
   
+  function Message(props) {
+    return (
+      <p>
+        You have read <strong>{props.count}</strong> advices so far.
+      </p>
+    )
+  }
+
+
+
 }
 
 export default App
