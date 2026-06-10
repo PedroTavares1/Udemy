b525d16226179d82417f4cb49abd96a8942048be
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-10 11:42:40 -0300

    ultimate-react-course-main Archives
    ultimate-react-course-main archives

--- The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/meu-projeto-react/src/App.jsx
@@ -1,45 +1,33 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+
+
+
+
 function App() {
  
-  const [advice, setAdvice] = useState('') // Estado para armazenar o conselho recebido da API
-  const [id, setId] = useState(0) // Estado para armazenar o ID do conselho recebido da API
-  const [count, setCount] = useState(0) // Estado para contar quantos conselhos foram lidos
+  const  []
 
 
-  async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice')
-    const data = await res.json()
-    console.log(data.slip.advice)
-    setId(data.slip.id)
-    setAdvice(data.slip.advice)
-    setCount((c) => c + 1)
-  }
-  
-  useEffect(function() {
-    getAdvice()
-  }, []) // O array vazio [] garante que a função seja executada apenas uma vez, quando o componente for montado.
- 
- return (
+
+
+
+
+
+
+
+
+
+
+
+  return (
     <div>
       <h1>Meu Projeto React</h1>
-      <button onClick={getAdvice}>Get advice</button>
-      <h2>{id} - {advice}</h2>
-      <Message count={count} />
+      <h2> Section 4 </h2>
+      <h2> 18. Destructuring Objects and Arrays</h2>
     </div>
   )
-  
-  function Message(props) {
-    return (
-      <p>
-        You have read <strong>{props.count}</strong> advices so far.
-      </p>
-    )
-  }
-
-
-
 }
 
 export default App
