e00a1a5dccaf7506cec012b0492e81b019b3effa
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-18 11:38:05 -0300

    Section 5 - Section Overview
    Section 5 - Section Overview

--- The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-React/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-React/src/App.jsx
@@ -4,12 +4,12 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+function App() { 
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+      <section id="center"> 
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -118,5 +118,7 @@ function App() {
     </>
   )
 }
+
+
 
 export default App
