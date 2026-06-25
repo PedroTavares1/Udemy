c43b474b852e512597050a047aa6cdcf04dd011d
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-25 11:55:17 -0300

    Project pizza-menu
    52. React Fragments

--- The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-React/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-React/src/App.jsx
@@ -77,11 +77,16 @@ function App() {
             <h2>Our Menu</h2>
 
             {numPizzas > 0 ? (
-                <ul className = "pizzas"> 
-                    {pizzas.map((pizza) => (
-                        <Pizza {...pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <React.Fragment> {/* or "<>"*/}
+                    <p> 
+                    Authentican Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious!
+                    </p>
+                    <ul className = "pizzas"> 
+                        {pizzas.map((pizza) => (
+                            <Pizza {...pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </React.Fragment> {/* or "</>"*/}
             ) : <p>We're still working on our menu. Please come back later :)</p>}
 
 
--- The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-udemy/src/index.js
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/03-pizza-menu-udemy/src/index.js
@@ -80,11 +80,17 @@ function Menu() {
             <h2>Our Menu</h2>
 
             {numPizzas > 0 ? (
-                <ul className = "pizzas"> 
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <React.Fragment> {/* or "<>"*/}
+                    <p> 
+                    Authentican Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious!
+                    </p>
+
+                    <ul className = "pizzas"> 
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </React.Fragment> {/* or "</>"*/}
             ) : <p>We're still working on our menu. Please come back later :)</p>}
 
 
