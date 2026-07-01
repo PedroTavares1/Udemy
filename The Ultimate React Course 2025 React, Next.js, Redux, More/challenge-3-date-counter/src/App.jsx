2bad64c78e2ce1cd31c0b679ebe35ec8eef0d5af
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-01 19:04:24 -0300

    CHALLENGE #1: Date Counter (v1)
    68. CHALLENGE #1: Date Counter (v1)

---
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/challenge-3-date-counter/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="date-counter">
+      console.log("Renderizou o componente");
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+export default App
+
