dc298e5a3f2a80c39983394a6ecf5b5fa98b8680
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-02 11:27:54 -0300

    68. CHALLENGE #1: Date Counter (v1)
    68. CHALLENGE #1: Date Counter (v1)

--- The Ultimate React Course 2025 React, Next.js, Redux, More/challenge-3-date-counter/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/challenge-3-date-counter/src/App.jsx
@@ -1,20 +1,43 @@
 import "./App.css";
+import { useState } from "react";
 
 
 function App() {
   return (
-    <div className="date-counter">
-      console.log("Renderizou o componente");
+    <div className="App">
+      <Counter />
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
 
 
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>    
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
 
-
-
-
+      
+      
+      
+    </div>
+  );
+}
+     
 
 export default App
 
