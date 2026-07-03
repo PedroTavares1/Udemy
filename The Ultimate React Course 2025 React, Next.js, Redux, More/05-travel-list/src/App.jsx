f20095cf3c7f6c4e27c981d08fc3e75d436ffcee
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-03 14:52:03 -0300

    New Project: The "Far Away" Travel List
    70. Building the Layout

--- The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
 
+
+function Logo() {
   return (
     <div>
-    
-    
+      <h1>🏝️ Far Away 💼 </h1>
     </div>
   )
 }
+
+function Form() {
+  return (
+    <div className="add-form">
+    <h3> What do you need for your 😍 trip? </h3>
+  </div>
+  )
+}
+
+function PackList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return <footer className="stats">
+    <em>💼 You have x items on your list, and you already packed X (X%)</em>
+  </footer>
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackList />
+      <Stats />
+    </div>
+  );
+}
+
+
+
 export default App
