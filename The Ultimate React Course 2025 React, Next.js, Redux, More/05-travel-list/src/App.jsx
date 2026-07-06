d5a6bf2267f95f350c6a87eb8b266f9b62b364ac
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-06 09:06:24 -0300

    The "Far Away" Travel List
    70. Building the Layout

--- The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 
@@ -30,12 +30,29 @@ function Logo() {
 }
 
 function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e);
+  }
+
+
   return (
-    <div className="add-form">
-    <h3> What do you need for your 😍 trip? </h3>
-  </div>
-  )
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3> What do you need for your 😍 trip? </h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button> + Add </button>
+  </form>
+  );
 } 
+
 
 function PackingList() {
   return (
