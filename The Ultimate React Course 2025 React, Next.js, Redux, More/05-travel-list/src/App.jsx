d46cc0f3cbafbb47bd6f5fbfd639d39e6b2832af
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-06 08:41:45 -0300

    The "Far Away" Travel List
    71. Rendering the Items List

--- The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/05-travel-list/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
 ];
 
 
@@ -49,7 +50,15 @@ function PackingList() {
 }
 
 function Item({ item }) {
-  return <li>{item.description}</li>
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {" "}
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  ) 
 }
 
 function Stats() {
