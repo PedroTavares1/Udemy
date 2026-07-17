d9fb4d0030a8f4d26f0c0fe9e76778aa609ac36f
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-17 15:12:08 -0300

    Sessão 7 - 05-travel-list - 90. The "children" Prop: Making a Reusable Button
    90. The "children" Prop: Making a Reusable Button

--- The Ultimate React Course 2025 React, Next.js, Redux, More/07-steps-continuacao/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/07-steps-continuacao/src/App.jsx
@@ -93,18 +93,16 @@ function Steps() {
             <Button 
               bgColor= "#7950f2"
               textColor="#fff"
-              onClick={handlePrevious}
-              text="Previous"
-              emoji="⬅️"
-            />            
+              onClick={handlePrevious}>
+              <span>⬅️</span> Previous              
+            </Button>            
 
             <Button 
               bgColor= "#7950f2"
               textColor="#fff"
-              onClick={handleNext}
-              text="Next"
-              emoji="➡️"
-            /> 
+              onClick={handleNext}>
+              Next <span>➡️</span> 
+              </Button> 
           </div>
         </div>
       )}
@@ -113,13 +111,13 @@ function Steps() {
 }
 
 
-function Button({ bgColor, textColor, onClick, text, emoji }) {
+function Button({ bgColor, textColor,onClick, children }) {
   return ( 
     <button
       style={{backgroundColor: bgColor, color: textColor}} 
       onClick={(onClick)}
     >
-      <span>{emoji}</span> {text}
+      {children}
     </button>
     
   )
