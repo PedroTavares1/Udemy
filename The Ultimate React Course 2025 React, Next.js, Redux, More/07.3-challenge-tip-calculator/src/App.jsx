e17da2adf3585d18a11a881f7cdf2144c10a880f
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-23 13:24:48 -0300

    93. CHALLENGE #1: Tip Calculator
    93. CHALLENGE #1: Tip Calculator Part 1

--- The Ultimate React Course 2025 React, Next.js, Redux, More/07.3-challenge-tip-calculator/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/07.3-challenge-tip-calculator/src/App.jsx
@@ -5,10 +5,43 @@ function App() {
   return (
     <div>
       <h1>Tip Calculator</h1>
-      
+      <Menu />
 
     </div>
   )
 }
+  function Menu() {
+    
+    const [bill, setBill] = useState("")
+    return (
+      <div>
+        <BillInput />
+        <Perguntas />
+        <Respostas />
+        <Button />
+      </div>
+      
+    )
+  }
+  
+function BillInput() {
+  
+  }
+
+  function Perguntas() {
+    
+  }
+
+  function Respostas() {
+
+
+    
+  }
+
+  function Button() {
+    
+  }
+
+
 
 export default App
