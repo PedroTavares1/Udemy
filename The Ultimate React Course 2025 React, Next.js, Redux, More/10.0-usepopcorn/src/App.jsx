e8cb87f8141bd8c59118f5bed4baff5aaba1d504
Pedro Tavares <pedroo.25@hotmail.com>
2026-07-29 19:11:32 -0300

    .
    .

--- The Ultimate React Course 2025 React, Next.js, Redux, More/10.0-usepopcorn/src/App.jsx
+++ The Ultimate React Course 2025 React, Next.js, Redux, More/10.0-usepopcorn/src/App.jsx
@@ -16,10 +16,10 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Get started</h1> 
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          </p> 
         </div>
         <button
           type="button"
